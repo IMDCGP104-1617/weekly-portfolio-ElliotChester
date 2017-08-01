f4ccfda35b967a2475ee241d45db9d33aa8645b6
Elliot Chester <elliotchester97@gmail.com>
2017-08-01 23:45:00 +0100

    Final Upload
    uploaded with the correct account

---
+++ Lists/Lists/Lists/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Stack<int> stack = new Stack<int>();
+            stack.Push(5);
+            stack.Push(4);
+            stack.Push(3);
+            stack.Push(2);
+            stack.Push(1);
+            while (!stack.IsEmpty())
+
+            {
+                int output;
+                stack.Pop(out output);
+                Console.WriteLine(output);
+            }*/
+
+            Queue<string> Queue = new Queue<string>();
+            Queue.EnQueue("Kill");
+            Queue.EnQueue("me");
+            Queue.EnQueue("please");
+            Queue.EnQueue("1");
+            Queue.EnQueue("2");
+            Queue.EnQueue("3");
+            Queue.EnQueue("4");
+            Queue.EnQueue("5");
+
+            while (!Queue.IsEmpty())
+
+            {
+                string output;
+                Queue.DeQueue(out output);
+                Console.WriteLine(output);
+            }
+
+
+            Console.Read();
+        }
+    }
+}
